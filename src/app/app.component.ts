6285e7f6bee4348509a6f339e7a2e551c90b9227
Huy Le <huyle.finance@gmail.com>
2018-10-09 18:09:36 -0400

    init with gsap installed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {Power1, Bounce} from 'gsap/all';
 
+declare var TweenMax: any;
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  templateUrl: './app.component.html'
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  @ViewChild('mushroom') box: ElementRef;
+
+  ngOnInit(): void {
+    this.doIt();
+  }
+
+  doIt(): void {
+    TweenMax.fromTo(this.box.nativeElement, 2, {x: 20}, {x: 440, ease: Power1.easeOut});
+    TweenMax.fromTo(this.box.nativeElement, 2, {y: 20}, {y: 440, ease: Bounce.easeOut});
+  }
 }
